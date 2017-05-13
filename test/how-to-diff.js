53c241a707832d86d8fe5cfa5d8948fe4bc1ab97
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2017-05-13 16:49:54 +0100

    renamed the var

--- test/how-to-diff.js
+++ test/how-to-diff.js
@@ -14,11 +14,10 @@ const source = {
 
 // an Introspected version of the same data
 // any operation will be known, no way to hide changes!
-const naked = Introspected(source, (naked, path) => {
+const intr = Introspected(source, (intr, path) => {
 
   // walk through the source
   let src = source;
-  let nkd = naked;
 
   // ensure objects/dictionaries won't throw
   let hOP = {}.hasOwnProperty;
@@ -35,54 +34,54 @@ const naked = Introspected(source, (naked, path) => {
       // this means it was added
       console.log('added ' + sub.join('.'));
 
-      // assign it from the naked
-      src[prop] = nkd[prop];
+      // assign it from the intr
+      src[prop] = intr[prop];
     }
-    // if the prop is in the current naked object
-    else if (prop in nkd) {
+    // if the prop is in the current intr object
+    else if (prop in intr) {
       // this means it was changed
       console.log('changed ' + sub.join('.'));
-      // There is no need to check src[prop] === nkd[prop]
+      // There is no need to check src[prop] === intr[prop]
       // because Introspected notifies changes *only* when
       // the new value is different from the previous one.
     }
-    // if property is not in the naked object
+    // if property is not in the intr object
     else {
       // it means it was removed
       delete src[prop];
       console.log('removed ' + sub.join('.'));
       // no need to keep walking through
-      // or accessing naked[prop] will set it again
+      // or accessing intr[prop] will set it again
       break; // or return;
     }
     src = src[prop];
-    nkd = nkd[prop];
+    intr = intr[prop];
   }
 });
 
 // change few fields
-naked.name = 'Andrea';
-naked.age = 39;
+intr.name = 'Andrea';
+intr.age = 39;
 
 // change a nested group
-naked.address = {
+intr.address = {
   street: 'Wenlock',
   city: 'London',
   country: 'UK'
 };
 
 // add new field
-naked.nationality = 'italian';
+intr.nationality = 'italian';
 
 // add nested property
-naked.address.postcode = 'N1';
+intr.address.postcode = 'N1';
 
 // remove a property
-delete naked.address.street;
+delete intr.address.street;
 
 // no notification will happen
 // because it's the same value
-naked.age = 39;
+intr.age = 39;
 
 // re set removed field
-naked.address.street = 'Archway';
+intr.address.street = 'Archway';
